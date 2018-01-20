65aaa2b50666484ab4aa6f918d401ab184551d8e
haru221 <gadget.tf@gmail.com>
2018-01-20 15:47:15 +0900

    Firebase Cirtification

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-
 import { StackNavitator } from 'react-navigation';
+import firebase from 'firebase';
 
 import Appbar from './src/components/Appbar';
 import MemoListScreen from './src/screens/MemoListScreen';
@@ -10,6 +10,18 @@ import MemoEditScreen from './src/screens/MemoEditScreen';
 import LoginScreen from './src/screens/LoginScreen';
 import SignUpScreen from './src/screens/SignUpScreen';
 import StackNavigator from 'react-navigation/src/navigators/StackNavigator';
+
+import ENV from './Env.json';
+
+const config = {
+  apiKey: ENV.FIREBASE_API_KEY,
+  authDomain: ENV.FIREBASE_AUTH_DOMAIN,
+  databaseURL: ENV.FIREBASE_DB_URL,
+  projectId: ENV.FIREBASE_PROJECT_ID,
+  storageBucket: ENV.FIREBASE_STORAGE,
+  messagingSenderId: ENV.FIREBASE_SENDER_ID
+};
+firebase.initializeApp(config);
 
 const App = StackNavigator({
   Login: {
